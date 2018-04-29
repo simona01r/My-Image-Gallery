2838c38ac3fdc746eab9a580554d7a423ae2ac96
Simona Adalberth <s.adalberth@gmail>
2018-04-29 17:39:58 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+ <h1>My Image Gallery
+========
+
+ <h2>Simple responsive image gallery, with <strong>jquery</strong>
+<br>
+
+<h3>In this project, the images of the Resideces I visited in Dubai:<br>
+<ul>
+	<li>can be categorized and linked to menu items</li>
+	<li>have an overlay effect  to change the background and show residence's info</li>
+	<li>will be opened with a lightbox when you click on the</li>
+	<li>have the ability to be scrolled through the lightbox </li>
+
+</ul>
+
+<h1>Short Description </h2>
+<h2>This project builds a portfolio image gallery with pictures from some Dubai Residences.
+<br><br>
---
+++ js/script.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+
+  $('nav a').on('click', function() {
+    // Current class assignment
+    $('nav li.current').removeClass('current');
+    $(this).parent().addClass('current');
+
+    // Set heading text
+    $('h1#heading').text($(this).text());
+
+    // Get & filter link text
+    var category = $(this).text().toLowerCase().replace(' ', '-');
+
+    // Remove hidden class if 'all-residences' is selected
+    if (category == 'all-residences') {
+      $('ul#gallery li:hidden').fadeIn('slow').removeClass('hidden');
+    } else {
+      $('ul#gallery li').each(function() {
+        if (!$(this).hasClass(category)) {
+          $(this).hide().addClass('hidden');
+        } else {
+          $(this).fadeIn('slow').removeClass('hidden');
+        }
+      });
+    }
+    // Stop link behavior
+    return false;
+  });
+
+// Mouseenter overlay
+  $('ul#gallery li').on('mouseenter', function(){
+    // Get data attribute values
+    var title = $(this).children().data('title');
+    var desc = $(this).children().data('desc');
+
+    // Validation
+    if(desc == null){
+      desc = 'Click To Enlarge';
+    }
+
+    if(title == null){
+      title = '';
+    }
+
+    // Create overlay div
+    $(this).append('<div class="overlay"></div>');
+
+    // Get the overlay div
+    var overlay = $(this).children('.overlay');
+
+    // Add HTML to overlay
+    overlay.html('<h3>'+title+'</h3></p>'+desc+'</p>');
+
+    // Fade in overlay
+    overlay.fadeIn(800);
+
+  });
+
+  // Mouseleave overlay
+  $('ul#gallery li').on('mouseleave', function(){
+    // Create overlay div
+    $(this).append('<div class="overlay"></div>');
+
+    // Get the overlay div
+    var overlay = $(this).children('.overlay');
+
+    // fade out overlay
+    overlay.fadeOut(500);
+  });
+
+});
